4ef303db92fcb522071783f4eb3e09cb6667f607
David K <dkerrigan@mailbox.org>
2024-02-19 14:53:05 -0500

    auto commit

--- p.cpp
+++ p.cpp
@@ -3,23 +3,4 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        int a[n];
-        int ans = 0;
-        for(int i = 0; i < n; ++i) cin >> a[i];
-        for(int k = 1; k <= n; ++k){
-            if(n%k == 0){
-                int g = 0;
-                for(int i = 0; i + k < n; ++i){
-                    g = gcd(g, abs(a[k+i]-a[i]));
-                }
-                if(g != 1) ans++;
-            }
-        }
-        cout << ans << endl;
-    }
 }
