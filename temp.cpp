dfa7b429ddb16fec04ba4b5d9ca35d612b6b0fb3
David <david19kerrigan@gmail.com>
2024-02-27 12:26:46 -0500

    auto commit

--- temp.cpp
+++ temp.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define fori for(int i = 0; i < n; ++i)
-#define forl for(ll i = 0; i < n; ++i)
+#define for(n) for(int i = 0; i < n; ++i)
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
