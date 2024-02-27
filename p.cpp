f755442a65dbf94776616bf4ad3f160b4169af30
David <david19kerrigan@gmail.com>
2024-02-27 16:00:40 -0500

    auto commit

--- p.cpp
+++ p.cpp
@@ -4,9 +4,13 @@ using namespace std;
 #define fori(n) for(int i = 0; i < n; ++i)
 #define forj(n) for(int j = 0; j < n; ++j)
 
+ll arr[10000][10000];
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int t; cin >> t;
     while(t--){
+        int n; cin >> n;
+        fori(n) forj(n) cin >> arr[i][j];
     }
 }
