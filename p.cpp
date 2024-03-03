33e7b2c98b4c58667816d4dad28bc9d22cd568cf
David K <dkerrigan@mailbox.org>
2024-03-03 17:20:47 -0500

    auto commit

--- p.cpp
+++ p.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define ll long long
 #define fori(s,n) for(int i = s; i < n; ++i)
 #define forj(s,n) for(int j = s; j < n; ++j)
-#define begend(a) arr.begin(), arr.end()
+#define bend(a) arr.begin(), arr.end()
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
@@ -13,12 +13,22 @@ int main() {
         vector<int> arr(n);
         vector<pair<int,int>> heap;
         fori(0,n) cin >> arr[i];
-        int max = *max_element(begend(arr));
-        heap.push_back(pair<int,int>(max, arr[max]));
+        int max = *max_element(bend(arr));
+        heap.push_back(pair<int,int>(arr[max], max));
         while(!heap.empty()){
-            pop_heap(begend(arr));
+            pop_heap(bend(arr));
             pair<int,int> top = heap[heap.size()-1];
             heap.pop_back();
+            int left = top.second-1;
+            int right = top.second+1;
+            if(left >= 0){
+                heap.push_back(pair<int,int>(arr[left], left));
+                push_heap(bend(heap));
+            }
+            if(right < n-1){
+                heap.push_back(pair<int,int>(arr[right], right));
+                push_heap(bend(heap));
+            }
         }
     }
 }
