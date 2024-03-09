b8d86d5b5b048935e5eb46452522f2e68650080f
David K <dkerrigan@mailbox.org>
2024-03-09 10:57:49 -0500

    auto commit

--- p.cpp
+++ p.cpp
@@ -4,25 +4,31 @@ using namespace std;
 #define fori(s,n) for(int i = s; i < n; ++i)
 #define forj(s,n) for(int j = s; j < n; ++j)
 #define bend(a) a.begin(), a.end()
-#define back(a,i) a[a.size()-i]
+#define aback(a,i) a[a.size()-i]
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int t; cin >> t;
     while(t--){
-        int realN; cin >> realN;
+        int n; cin >> n;
         string str; cin >> str;
-        int pos = 0;
-        int n = realN;
-        while(pos < realN){
-            int opp = str[n-pos-1]; 
-            if(opp == str[pos]){ 
-                n += 2;
-            }
-            pos++;
+        int c1 = 0; int c0 = 0;
+        fori(0,n){
+            if(str[i] == '1') c1++; 
+            if(str[i] == '0') c0++;
         }
-        string prefix = str.substr(0, n-realN);
-        fori(0, prefix.length()) cout << prefix[i] << " ";
-        cout << endl;
+        if(c1 != c0){ 
+            cout << -1 << endl;
+            break;
+        }
+        vector<int> ans;
+        deque<char> q;
+        fori(0,n) q.push_back(str[i]);
+        while(!q.empty()){
+            while(!q.empty() && q.front() != q.back()){
+                q.pop_back();
+                q.pop_front();
+            }
+        }
     }
 }
--- temp.cpp
+++ temp.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define fori(s,n) for(int i = s; i < n; ++i)
 #define forj(s,n) for(int j = s; j < n; ++j)
 #define bend(a) a.begin(), a.end()
-#define back(a,i) a[a.size()-i]
+#define aback(a,i) a[a.size()-i]
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
