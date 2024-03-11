a06454e372383e3ebf9f8d58d94c4824eac4712a
David K <dkerrigan@mailbox.org>
2024-03-10 20:03:19 -0400

    auto commit

--- practice/156b.cpp
+++ practice/156b.cpp
@@ -15,14 +15,13 @@ int main() {
         double a_p = sqrt(pow(px-ax,2) + pow(py-ay,2));
         double b_p = sqrt(pow(px-bx,2) + pow(py-by,2));
         double a_b = (sqrt(pow(ax-bx,2) + pow(ay-by,2))) / 2.0;
-        double a = sqrt(pow(ax,2) + pow(ay,2));
-        double b = sqrt(pow(bx,2) + pow(by,2));
+        double a_o = sqrt(pow(ax,2) + pow(ay,2));
+        double b_o = sqrt(pow(bx,2) + pow(by,2));
         double ans = INT_MAX;
-        if(a_p > a) ans = min(ans, a_p);
-        if(b_p > b) ans = min(ans, b_p);
-        if(b > b_p) ans = min(ans, b);
-        if(a > a_p) ans = min(ans, a);
-        if(a_b > a || a_b > b) ans = min(ans, a_b);
+        ans = min(ans, max(a_p, a_o));
+        ans = min(ans, max(b_p, b_o));
+        ans = min(ans, max({a_b, b_o, a_p}));
+        ans = min(ans, max({a_b, a_o, b_p}));
         cout << setprecision(10) << ans << endl;
     }
 }
