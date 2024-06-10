9712498b5ca21615be52d8161f40398e5c581f23
David K <dkerrigan@mailbox.org>
2024-06-10 17:59:09 -0400

    auto commit

--- p.cpp
+++ p.cpp
@@ -12,5 +12,8 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int t; cin >> t;
     while(t--){
+        int n; cin >> n;
+        vector<int> b(n);
+        fori(0,n) cin >> b[i];
     }
 }
