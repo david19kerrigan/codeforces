1b1ebb83ff85f3dcf7029c1665a790c734a122f1
david19kerrigan <david19kerrigan@gmail.com>
2025-07-31 21:24:16 -0400

    count

--- p.cpp
+++ p.cpp
@@ -8,59 +8,14 @@ using namespace std;
 #define print_map(map) for(auto cur: map) cout << cur.first << ": " << cur.second << endl;
 #define print_set(set) for(auto cur: set) { cout << cur << " "; } cout << endl;
 
-string base(ll number, ll bs){
-    if(number == 0) return 0;
-    string result;
-    while(number > 0){
-        result = to_string(number % bs) + result;
-        number /= bs;
-    }
-    return result;
-}
-
-bool isP(string snum){
-    string snum2 = snum;
-    reverse(all(snum2));
-    return snum == snum2;
-}
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    ll a, n;
-    cin >> a >> n;
-    ll ans = 0;
-    ll len = 1;
-    vector<ll> pow = {1};
-    while(true){
-        if(pow.size() < len) pow.push_back(pow.back() * 10);
-        if(pow.back() > n) break;
-        vector<ll> dig((len+1)/2, 0);
-        dig[0] = 1;
-        while(true){
-            ll num = 0;
-            rep(i, len){
-                ll ind;
-                if(i < dig.size()) ind = i;
-                else ind = len - 1 - i;
-                num += dig[ind] * pow[i];
-            }
-            if(isP(base(num, a)) and num <= n){
-                ans += num;
-            }
-            bool found = false;
-            for(ll i = dig.size() - 1; i >= 0; --i){
-                if(dig[i] == 9){
-                    dig[i] = 0;
-                }
-                else{
-                    ++dig[i];
-                    found = true;
-                    break;
-                }
-            }
-            if(!found) break;
-        }
-        ++len;
+    int n, m;
+    cin >> n >> m;
+    vector<int> x_white(1e9+1), y_white(1e9+1);
+    rep(i, m){
+        int x, y; char c;
+        cin >> x >> y >> c;
     }
-    cout << ans << endl;
 }
